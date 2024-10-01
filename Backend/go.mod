91fc45c4440f60a894abca5399f7b82d4ec4005a
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-10-01 13:37:46 +0700

    Update go module

--- Backend/go.mod
+++ Backend/go.mod
@@ -1,9 +1,5 @@
-module github.com/AndreanDjabbar/Andrean-Portofolio/server
+module github.com/AndreanDjabbar/web-portfolio-react/Backend
 
 go 1.22.1
 
-require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
-)
+require github.com/joho/godotenv v1.5.1
--- Backend/main.go
+++ Backend/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"github.com/AndreanDjabbar/Andrean-Portofolio/server/feedbackService"
+
+	"github.com/AndreanDjabbar/web-portfolio-react/Backend/feedbackService"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load .env file hanya jika tidak dijalankan di Railway
 	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
 		err := godotenv.Load()
 		if err != nil {
@@ -17,16 +17,14 @@ func main() {
 		}
 	}
 
-	// Ambil port dari environment variable (disediakan oleh Railway)
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"  // Default fallback port jika tidak ada variabel PORT
+		port = "8080"  
 	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/feedback", feedbackService.FeedbackHandler)
 
-	// Menampilkan pesan log dengan port yang tepat
 	fmt.Println("Listening on port:", port)
 	err := http.ListenAndServe("0.0.0.0:" + port, mux)
 	if err != nil {
